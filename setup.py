4c9748e424f6cdc76f408ef6d66017dc638c989f
Aykut Bulgu <mabulgu@gmail.com>
2020-06-10 20:31:10 +0300

    update gitignore && change dependent packages && change setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
 
+with open("README.adoc", "r") as file:
+    long_description = file.read()
+
 setup(
-    name='kfk',
-    version='0.1',
-    py_modules=['kfk_cli'],
+    name='strimzi-kafka-cli',
+    version='0.1.0-rc1',
+    description="Command Line Interface for Strimzi Kafka Operator",
+    long_description=long_description,
+    long_description_content_type="text/asciidoc",
+    py_modules=['strimzi_kafka_cli'],
     install_requires=[
         'Click',
+        'PyYAML',
+        'wget',
     ],
     entry_points='''
         [console_scripts]
-        kfk=kfk_cli:kfk
+        kfk=strimzi_kafka_cli:kfk
     ''',
+
+    url="https://github.com/systemcraftsman/strimzi-kafka-cli",
+    python_requires='>=3.7',
 )
