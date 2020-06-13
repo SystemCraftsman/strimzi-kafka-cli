1178f6601d3ded14247c1597e5c6f6927c8b84c4
Aykut Bulgu <mabulgu@gmail.com>
2020-06-13 21:57:11 +0300

    fix comparison becasuse of linting

--- kfk/constants.py
+++ kfk/constants.py
@@ -10,8 +10,9 @@ STRIMZI_PATH = BASE_PATH + "/strimzi-{version}".format(version=STRIMZI_VERSION)
 STRIMZI_RELEASE_URL = "https://github.com/strimzi/strimzi-kafka-operator/releases/download/{version}/strimzi-{version}.tar.gz".format(
     version=STRIMZI_VERSION)
 KUBECTL_VERSION = "v1.18.0"
-KUBECTL_PATH = BASE_PATH + ("/kubectl" if platform.system().lower() != "windows" else "kubectl.exe")
+KUBECTL = "kubectl" if platform.system().lower() != "windows" else "kubectl.exe"
+KUBECTL_PATH = BASE_PATH + "/" + KUBECTL
 PROCESSOR_TYPE = "amd64" if sys.maxsize > 2 ** 32 else "386"
-KUBECTL_RELEASE_URL = "https://storage.googleapis.com/kubernetes-release/release/{version}/bin/{operating_system}/{processor_type}/kubectl".format(
-    version=KUBECTL_VERSION, operating_system=platform.system().lower(), processor_type=PROCESSOR_TYPE)
+KUBECTL_RELEASE_URL = "https://storage.googleapis.com/kubernetes-release/release/{version}/bin/{operating_system}/{processor_type}/{kubectl}".format(
+    version=KUBECTL_VERSION, operating_system=platform.system().lower(), processor_type=PROCESSOR_TYPE, kubectl=KUBECTL)
 SPACE = " "
