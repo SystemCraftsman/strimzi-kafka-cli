8f8b3f5fe2cd5e8ded28b97c9424a5a532fbf707
Aykut Bulgu <mabulgu@gmail.com>
2020-10-22 00:54:25 +0300

    issue-13

--- kfk/clusters_command.py
+++ kfk/clusters_command.py
@@ -1,15 +1,19 @@
 import click
 import os
+import yaml
 
 from kfk.command import kfk
 from kfk.option_extensions import NotRequiredIf
-from kfk.commons import print_missing_options_for_command, get_resource_as_stream, open_file_in_system_editor, \
-    create_temp_file
+from kfk.commons import *
 from kfk.kubectl_command_builder import Kubectl
 from kfk.config import *
 
 
 @click.option('-n', '--namespace', help='Namespace to use')
+@click.option('--delete-config', help='A cluster configuration override to be removed for an existing cluster',
+              multiple=True)
+@click.option('--config', help='A cluster configuration override for the cluster being altered.',
+              multiple=True)
 @click.option('--alter', help='Alter the cluster.', is_flag=True)
 @click.option('--delete', help='Delete the cluster.', is_flag=True)
 @click.option('--create', help='Create the cluster.', is_flag=True)
@@ -20,7 +24,7 @@ from kfk.config import *
 @click.option('--list', help='List all available clusters.', required=True, is_flag=True)
 @click.option('--cluster', help='Cluster Name', required=True, cls=NotRequiredIf, not_required_if=['list'])
 @kfk.command()
-def clusters(cluster, list, create, describe, delete, alter, output, namespace):
+def clusters(cluster, list, create, describe, delete, alter, config, delete_config, output, namespace):
     """The kafka cluster(s) to be created, altered or described. """
     if list:
         os.system(Kubectl().get().kafkas().namespace(namespace).build())
@@ -45,6 +49,31 @@ def clusters(cluster, list, create, describe, delete, alter, output, namespace):
         if is_confirmed:
             os.system(Kubectl().delete().kafkas(cluster).namespace(namespace).build())
     elif alter:
-        os.system(Kubectl().edit().kafkas(cluster).namespace(namespace).build())
+        if len(config) > 0 or len(delete_config) > 0:
+            if resource_exists("kafkas", cluster, cluster, namespace):
+                stream = get_resource_as_stream("kafkas", cluster, namespace)
+                cluster_dict = yaml.full_load(stream)
+
+                delete_last_applied_configuration(cluster_dict)
+
+                if len(config) > 0:
+                    if cluster_dict["spec"]["kafka"].get("config") is None:
+                        cluster_dict["spec"]["kafka"]["config"] = {}
+                    add_resource_kv_config(config, cluster_dict["spec"]["kafka"]["config"])
+
+                if len(delete_config) > 0:
+                    if cluster_dict["spec"]["kafka"].get("config") is not None:
+                        delete_resource_config(delete_config, cluster_dict["spec"]["config"])
+
+                cluster_yaml = yaml.dump(cluster_dict)
+                cluster_temp_file = create_temp_file(cluster_yaml)
+                os.system(
+                    Kubectl().apply().from_file("{cluster_temp_file_path}").namespace(namespace).build().format(
+                        cluster_temp_file_path=cluster_temp_file.name))
+                cluster_temp_file.close()
+            else:
+                print_resource_not_found_msg(cluster, namespace)
+        else:
+            os.system(Kubectl().edit().kafkas(cluster).namespace(namespace).build())
     else:
         print_missing_options_for_command("clusters")
--- tests/test_clusters_command.py
+++ tests/test_clusters_command.py
@@ -44,10 +44,29 @@ class TestKfkClusters(TestCase):
             Kubectl().get().kafkas(self.cluster).namespace(self.namespace).output("yaml").build())
 
     @mock.patch('kfk.clusters_command.os')
-    def test_alter_cluster(self, mock_os):
+    def test_alter_cluster_without_config(self, mock_os):
         result = self.runner.invoke(kfk, ['clusters', '--alter', '--cluster', self.cluster, '-n', self.namespace])
         assert result.exit_code == 0
         mock_os.system.assert_called_with(Kubectl().edit().kafkas(self.cluster).namespace(self.namespace).build())
+
+    @mock.patch('kfk.clusters_command.create_temp_file')
+    @mock.patch('kfk.commons.get_resource_yaml')
+    @mock.patch('kfk.clusters_command.resource_exists')
+    @mock.patch('kfk.clusters_command.os')
+    def test_alter_cluster_with_config(self, mock_os, mock_resource_exists, mock_get_resource_yaml,
+                                     mock_create_temp_file):
+        mock_resource_exists.return_value = True
+        with open(r'files/yaml/kafka-ephemeral.yaml') as file:
+            topic_yaml = file.read()
+            mock_get_resource_yaml.return_value = topic_yaml
+            result = self.runner.invoke(kfk, ['clusters', '--alter', '--config', 'unclean.leader.election.enable=true',
+                                              '--cluster', self.cluster, '-n', self.namespace])
+            assert result.exit_code == 0
+
+            with open(r'files/yaml/kafka-ephemeral_with_one_config.yaml') as file:
+                expected_topic_yaml = file.read()
+                result_topic_yaml = mock_create_temp_file.call_args[0][0]
+                assert expected_topic_yaml == result_topic_yaml
 
     @mock.patch('kfk.clusters_command.click.confirm')
     @mock.patch('kfk.clusters_command.os')
