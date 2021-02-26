b0f2fb04f59fd0830c0f7556b107a9a2e603f8ce
Aykut Bulgu <mabulgu@gmail.com>
2021-02-27 00:19:58 +0300

    add create for connect cluster

--- kfk/connect_command.py
+++ kfk/connect_command.py
@@ -8,7 +8,8 @@ from kfk.commons import *
 
 @click.option('-n', '--namespace', help='Namespace to use', required=True)
 @click.option('--cluster', help='Connect cluster name')
+@click.option('--create', 'is_create', help='Create a new connect cluster.', is_flag=True)
 @kfk.command()
-def connect(cluster, namespace):
+def connect(is_create, cluster, namespace):
     """Creates, alters, deletes, describes Kafka connect cluster(s) or connectors."""
     print_missing_options_for_command("connect")
--- tests/test_connect_command.py
+++ tests/test_connect_command.py
@@ -8,10 +8,15 @@ class TestKfkConnect(TestCase):
 
     def setUp(self):
         self.runner = CliRunner()
-        self.cluster = "my-cluster"
+        self.cluster = "my-connect-cluster"
         self.namespace = "kafka"
 
     def test_no_option(self):
         result = self.runner.invoke(kfk, ['connect', '-n', self.namespace])
         assert result.exit_code == 0
         assert "Missing options: kfk connect" in result.output
+
+    @mock.patch('kfk.clusters_command.os')
+    def test_create_cluster(self, mock_os):
+        result = self.runner.invoke(kfk, ['connect', '--create', '--cluster', self.cluster, '-n', self.namespace])
+        assert result.exit_code == 0
